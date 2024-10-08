af66db71be3c19a18b19e2fd68640501051ccbc7
Juan Sánchez <JuanchoSL@hotmail.com>
2024-10-08 21:41:49 +0200

    try fix comments

--- src/Drivers/CSSMin.php
+++ src/Drivers/CSSMin.php
@@ -6,48 +6,8 @@ namespace JuanchoSL\AssetMinifyer\Drivers;
 
 use JuanchoSL\AssetMinifyer\Contracts\MinifyerInterface;
 
-class CSSMin implements MinifyerInterface
+class CSSMin extends AbstractMin implements MinifyerInterface
 {
-
-    /**
-     * Elimina los comentarios de una cadena, que puede ser código php
-     * @param string $str Cadena a ser limpiada
-     * @return string cadena limpiada sin comentarios
-     */
-    protected function removeComments(string $str): string
-    {
-        $str = preg_replace(
-            array(
-                // eliminate single line comments in '// ...' form
-                '#^\s*//(.+)$#m',
-                // eliminate multi-line comments in '/* ... */' form, at multiline
-                '#\s*/\*(.+)\*/\s*#Us',
-                // eliminate multi-line comments in '/* ... */' form, at start of string
-                '#^\s*/\*(.+)\*/#Us',
-                // eliminate multi-line comments in '/* ... */' form, at end of string
-                '#/\*(.+)\*/\s*$#Us'
-            ),
-            '',
-            $str
-        );
-        // eliminate extraneous space
-        return trim($str ?? '');
-    }
-
-    protected function removeEndLines(string $str): string
-    {
-        $str = preg_replace(array("/(\r|\n|\t)/", "/\s{2,}/"), " ", $str);
-        return trim($str ?? '');
-    }
-
-    protected function removeExtraSpaces(string $str): string
-    {
-        foreach ([': ', '{ ', ' }', '; ', ' :', ' {', '} ', ' ;', ', ', ' ,', ' "', '" ', " '", "' ", " (", " )", "( ", ") ", " =", "= "] as $search) {
-            $str = str_replace($search, trim($search), $str);
-        }
-        $str = str_replace(' and(', ' and (', $str);
-        return trim($str);
-    }
 
     public function min(string $content): string
     {
--- src/Drivers/JShrinkMin.php
+++ src/Drivers/JShrinkMin.php
@@ -7,13 +7,22 @@ namespace JuanchoSL\AssetMinifyer\Drivers;
 use JuanchoSL\AssetMinifyer\Contracts\MinifyerInterface;
 use JShrink\Minifier;
 
-class JShrinkMin implements MinifyerInterface
+class JShrinkMin extends AbstractMin implements MinifyerInterface
 {
 
+    public function min(string $content): string
+    {
+        $content = Minifier::minify($content);
+        $content = $this->removeComments($content);
+        //$content = $this->removeEndLines($content);
+        //$content = $this->removeExtraSpaces($content);
+        return $content;
+    }
 
     public static function minify(string $content): string
     {
-        return Minifier::minify($content);
+        $obj = new JShrinkMin();
+        return $obj->min($content);
     }
 
 }
